c970942db8d9caabe2b445c61891e439ef2c01d9
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2023-11-08 23:19:22 -0500

    '[ui] Add validation for CPU Set and CPUs in validateInputs function'
    Signed-off-by: NaveenKumar Namachivayam <catch.nkn@gmail.com>

---
+++ ui/src/ContainerResources.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogActions from '@mui/material/DialogActions';
+import Button from '@mui/material/Button';
+
+export default function ContainerResources() {
+    return (
+        <></>
+    )
+}
